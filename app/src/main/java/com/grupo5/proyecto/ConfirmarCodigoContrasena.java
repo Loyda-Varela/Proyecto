24add6926602dc7777746a5592d420c5b44c24da
Loyda-Varela <73678119+Loyda-Varela@users.noreply.github.com>
2022-07-06 23:45:31 -0600

    Initial commit

---
+++ app/src/main/java/com/grupo5/proyecto/ConfirmarCodigoContrasena.java
@@ -0,0 +1,14 @@
+package com.grupo5.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmarCodigoContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_codigo_contrasena);
+    }
+}
